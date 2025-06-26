75aeba96a54440d004d35c197f7983d7df6fa04a
phamdinhthanhcntt6 <thanh236200210a@gmail.com>
2025-06-26 22:48:51 +0700

    feat: set up Express server with MongoDB connection and environment variables
    - Added dotenv for environment variable management
    - Integrated mongoose for MongoDB connection
    - Created app.ts for Express application setup with CORS and JSON parsing
    - Updated package.json to include necessary dependencies and types
    - Created .env file for configuration of PORT, MONGO_URI, and JWT settings

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import mongoose from 'mongoose'
+import app from '~/app'
+
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI || ''
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
